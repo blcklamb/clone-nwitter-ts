360a43766261c64f6195e154fd159d49f86c7b53
chaejung Kim <whenucan35@gmail.com>
2022-07-20 13:38:14 +0900

    Add: Router setting

---
+++ nwitter-ts/src/routes/Router.style.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const RouterContainer = styled.div`
+    max-width: 890;
+    width: 100%;
+    margin: 0 auto;
+    margin-top: 80;
+    display: flex;
+    justify-content: center;
+`
---
+++ nwitter-ts/src/routes/Router.tsx
@@ -0,0 +1,19 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Auth from 'components/Auth/Auth';
+
+import {
+    RouterContainer
+} from './Router.style'
+
+
+export default function Router({ }) {
+  return (
+    <HashRouter>
+        <RouterContainer>
+            <Routes>
+                <Route path="/" element={<Auth />}/>
+            </Routes>
+        </RouterContainer>
+    </HashRouter>
+  )
+}
