044dd977007e30c9f66522b0b594e5bdf799ea61
chaejung Kim <whenucan35@gmail.com>
2022-07-21 11:37:56 +0900

    Update: Router.tsx props add

--- nwitter-ts/src/routes/Router.tsx
+++ nwitter-ts/src/routes/Router.tsx
@@ -1,14 +1,30 @@
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import Auth from 'components/Auth/Auth'
+import Navigation from 'components/Navigation/Navigation'
 
 import { RouterContainer } from './Router.style'
 
-export default function Router({}) {
+export default function AppRouter({
+  isLoggedIn,
+  userObj,
+  refreshUser,
+}: {
+  isLoggedIn: boolean
+  userObj: object
+  refreshUser: () => void
+}) {
   return (
     <HashRouter>
+      {isLoggedIn && <Navigation userObj={userObj} />}
       <RouterContainer>
         <Routes>
-          <Route path="/" element={<Auth />} />
+          {isLoggedIn ? (
+            <>
+              <p>Hi you are logged in</p>
+            </>
+          ) : (
+            <Route path="/" element={<Auth />} />
+          )}
         </Routes>
       </RouterContainer>
     </HashRouter>
