f2b8a8253dded24a08470b6f7a6c122e362a537c
chaejung Kim <whenucan35@gmail.com>
2022-07-21 10:24:34 +0900

    Chore: prettier apply

--- nwitter-ts/src/routes/Router.tsx
+++ nwitter-ts/src/routes/Router.tsx
@@ -1,19 +1,16 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import Auth from 'components/Auth/Auth';
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Auth from 'components/Auth/Auth'
 
-import {
-    RouterContainer
-} from './Router.style'
+import { RouterContainer } from './Router.style'
 
-
-export default function Router({ }) {
+export default function Router({}) {
   return (
     <HashRouter>
-        <RouterContainer>
-            <Routes>
-                <Route path="/" element={<Auth />}/>
-            </Routes>
-        </RouterContainer>
+      <RouterContainer>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+        </Routes>
+      </RouterContainer>
     </HashRouter>
   )
 }
