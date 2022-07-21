e5d741dbf57a0c90fa97f118f29886bd8387f358
chaejung Kim <whenucan35@gmail.com>
2022-07-21 16:49:34 +0900

    Add: Home.tsx but type error remain

---
+++ nwitter-ts/src/components/Home/Home.style.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+  width: 100%;
+  max-width: 320px;
+  display: flex;
+  flex-direction: column;
+`
---
+++ nwitter-ts/src/components/Home/Home.tsx
@@ -0,0 +1,41 @@
+import { dbService } from 'components/Auth/fbase'
+import { useEffect, useState } from 'react'
+//import Nweet from 'components/Nweet'
+//import NweetFactory from 'components/NweetFactory'
+
+import { HomeContainer } from './Home.style'
+
+function Home({ userObj }: { userObj: any }) {
+  const [nweets, setNweets] = useState([])
+
+  useEffect(() => {
+    dbService
+      .collection('nweets')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(snapshot => {
+        const newArray:{ id: string; }[]= snapshot.docs.map(document => ({
+          id: document.id,
+          ...document.data(),
+        }))
+        setNweets(newArray:{ id: string; }[])
+      })
+  }, [])
+  return (
+    <HomeContainer>
+      {/* <NweetFactory userObj={userObj}/> */}
+      <div style={{ margin: 30 }}>
+        {nweets.map(nweet => {
+          return (
+            // <Nweet
+            //   key={nweet.id}
+            //   nweetObj={nweet}
+            //   isOwner={nweet.creatorId === userObj.uid}
+            // />
+          )
+        })}
+      </div>
+    </HomeContainer>
+  )
+}
+
+export default Home
