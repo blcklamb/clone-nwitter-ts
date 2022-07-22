049b3ba32547327653512f8d357195944df6d10d
chaejung Kim <whenucan35@gmail.com>
2022-07-22 16:24:52 +0900

    Temp: updateProfile error unsolved

--- nwitter-ts/src/components/App.tsx
+++ nwitter-ts/src/components/App.tsx
@@ -6,25 +6,39 @@ import type firebase from 'firebase/compat/app'
 
 //??
 interface UserObjProps {
-  uid: (firebase.UserInfo | null)[]
+  uid: (firebase.UserInfo | null)[] | string
   displayName: string | null
-  updateProfile: (args: any) => Promise<void>
+  //   updateProfile: (profile: {
+  //     displayName?: string | null | undefined;
+  //     photoURL?: string | null | undefined;
+  // }) => Promise<void>
 }
 
 function App() {
   const [init, setInit] = useState(false)
-  const [userObj, setUserObj] = useState<any>([])
+  const [userObj, setUserObj] = useState<UserObjProps>({
+    uid: [],
+    displayName: null,
+    // updateProfile: (profile: {
+    //   displayName?: "",
+    //   photoURL?: "",
+    // }),
+  })
 
   useEffect(() => {
     authService.onAuthStateChanged(user => {
       if (user) {
+        const displayName = user?.displayName
+        const photoURL = user?.photoURL
+        user.updateProfile({ displayName, photoURL })
+
         setUserObj({
           uid: user?.providerData,
           displayName: user?.displayName,
-          updateProfile: (args: any) => user?.updateProfile(args),
+          // updateProfile: args => user?.updateProfile(args),
         })
       } else {
-        setUserObj([])
+        setUserObj({ uid: [], displayName: null })
       }
       setInit(true)
     })
@@ -32,20 +46,18 @@ function App() {
 
   const refreshUser = () => {
     const user = authService.currentUser
-    setUserObj({
-      uid: user?.uid,
-      displayName: user?.displayName,
-      updateProfile: (args: any) => user?.updateProfile(args),
-    })
+    if (user) {
+      setUserObj({
+        uid: user?.uid,
+        displayName: user?.displayName,
+        // updateProfile: (args: any) => user?.updateProfile(args),
+      })
+    }
   }
   return (
     <>
       {init ? (
-        <AppRouter
-          refreshUser={refreshUser}
-          isLoggedIn={Boolean(userObj)}
-          userObj={userObj}
-        />
+        <AppRouter refreshUser={refreshUser} isLoggedIn={Boolean(userObj)} userObj={userObj} />
       ) : (
         '로딩 중입니다...'
       )}
